863ad0ae96186a2fd9b7a8aa434c27b7a33118e8
Affan2k3 <affan2k3@gmail.com>
2023-12-18 22:27:57 +0500

    Hoisting understood

--- learning.js
+++ learning.js
@@ -1,4 +1,12 @@
-console.log(a(2, 3));
-function a(c, d) {
-  return c + d;
-}
+// console.log(a(2, 3));
+// function a(c, d) {
+//   return c + d;
+// }
+
+// const b = (c, d) => c + d;
+// console.log(b(3, 4));
+
+console.log(a);
+var a = 20;
+
+// if it is var it will not give console.error But it will give undefined;

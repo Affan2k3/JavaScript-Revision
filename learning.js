0a04bd4f9ffe9a85559c327eb310251b4bb7449b
Affan2k3 <affan2k3@gmail.com>
2023-12-19 22:54:42 +0500

    null is 0

--- learning.js
+++ learning.js
@@ -1,14 +1,16 @@
-
-console.log(a);
-var a = 20;
+// console.log(a);
+// var a = 20;
 
 // if it is var it will not give console.error But it will give undefined;
 
-var a = null 
+var a = null;
 
 // it is null
 
-let a;
+let b;
 
 // it is undefined
 
+var g = null;
+
+console.log(Number(g));

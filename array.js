c1aba43e9d5f09dfa9eb6142dc18c428d26d06ce
Affan2k3 <affan2k3@gmail.com>
2023-12-20 21:11:50 +0500

    arrays

--- array.js
+++ array.js
@@ -1,5 +1,18 @@
-let array = new Array(6);
+// let array = new Array(6);
 
-array[3] = "Hello";
+// array[3] = "Hello";
+// array[3] = "naa";
+// console.log(array);
 
-console.log(array);
+// let fruits = ["Apple", "Orange", "Plum"];
+
+// console.log(fruits[fruits.length - 2]);
+// console.log(fruits.length);
+
+// let myArray = new Array("1", "2", "3");
+
+// myArray = myArray.concat("a", "b", "c");
+
+// myArray.push("HELLO");
+
+// console.log(myArray);

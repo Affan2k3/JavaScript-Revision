44b869a1741862a429cb01145589758c8fcff712
Affan2k3 <affan2k3@gmail.com>
2023-12-23 22:28:04 +0500

    loops

---
+++ loops.js
@@ -0,0 +1,37 @@
+// let i = 0;
+
+// while (i < 6) {
+//   if (i === 3) {
+//     break;
+//   }
+//   i = i + 1;
+// }
+
+// console.log(i, i);
+// // Expected output: 3
+// //
+
+const food = "sushi";
+
+switch (food) {
+  case "sushi":
+    console.log("Sushi is originally from Japan.");
+    break;
+  case "pizza":
+    console.log("Pizza is originally from Italy.");
+    break;
+  default:
+    console.log("I have never heard of that dish.");
+    break;
+}
+
+const age = 19;
+
+switch (age) {
+  case 20:
+    console.log("HELLP");
+
+  case 19:
+    console.log("AAJH");
+    break;
+}

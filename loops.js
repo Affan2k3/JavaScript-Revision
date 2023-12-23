cef5cd5cd8c720968991630fd330f8a67d8b3c51
Affan2k3 <affan2k3@gmail.com>
2023-12-23 22:30:38 +0500

    till control flow

--- loops.js
+++ loops.js
@@ -35,3 +35,21 @@ switch (age) {
     console.log("AAJH");
     break;
 }
+
+// console.log("Pizza is originally from Italy.");
+// break;
+// default:
+// console.log("I have never heard of that dish.");
+// break;
+// }
+
+// const age = 19;
+
+// switch (age) {
+// case 20:
+// console.log("HELLP");
+
+// case 19:
+// console.log("AAJH");
+// break;
+// }

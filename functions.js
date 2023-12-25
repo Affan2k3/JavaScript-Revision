c7f39e4d7a1732643d748856ed84284a8e1a6b59
Affan2k3 <affan2k3@gmail.com>
2023-12-25 19:33:34 +0500

    function expressions

---
+++ functions.js
@@ -0,0 +1,35 @@
+// function name() {
+//     console.log("HELLO")
+//     return "naa"
+// }
+
+// name()
+// myFunc(mycar);
+
+//     function myFunc(theObject) {
+//     theObject.make = "Toyota";
+//      }
+  
+//   const mycar = {
+//     make: "Honda",
+//     model: "Accord",
+//     year: 1998,
+//   };
+  
+// console.log(mycar.make); // "Honda"
+  
+  
+//   console.log(mycar)
+// console.log(mycar.make); // "Toyota"
+
+// FUNCTION EXPRESSIONS
+
+console.log(hello())
+
+ const hello =  function name() {
+    
+    console.log("NAAA")
+
+    
+ }
+

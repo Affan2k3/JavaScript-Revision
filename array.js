2e452d365bf15bce8d3e27b3994ad1f3a1c6e555
Affan2k3 <affan2k3@gmail.com>
2023-12-25 12:16:41 +0500

    string iteration

--- array.js
+++ array.js
@@ -20,18 +20,20 @@
 const array = [1, 2, 3, 4, 5,6,7,8,9]
 
 for (number of array) {
-  if (number === 3) {
-    continue
-  }
 
-  if (number === 5) {
-    break
-  }
 
-  console.log(number)
+  // console.log(number)
 }
 
 
+const iterable = "HELLOOO";
+
+for (const value of iterable) {
+  console.log(value);
+}
+// "b"
+// "o"
+// "o"
 
 
 
